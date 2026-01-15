ee75b6ad10739f642a5a2aba01efd57cc2b3efe5
profe-pep <jjimen2@xtec.cat>
2026-01-15 15:45:02 +0100

    Fix avatar issue

--- bloggy/database/factories/UserFactory.php
+++ bloggy/database/factories/UserFactory.php
@@ -23,12 +23,15 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $imageUrl = "https://picsum.photos/seed/{seed}/300/300";
+
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'avatar'    => str_replace("{seed}", fake()->uuid(), $imageUrl),
         ];
     }
 
--- bloggy/database/seeders/UserSeeder.php
+++ bloggy/database/seeders/UserSeeder.php
@@ -13,22 +13,18 @@ class UserSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        $imageUrl = "https://picsum.photos/seed/{seed}/300/300";
-
+    {   
         // Admin
         User::factory()->create([
             'name'      => config('admin.name'),
             'email'     => config('admin.email'),
             'password'  => Hash::make(config('admin.password')),
-            'avatar'    => str_replace("{seed}", fake()->uuid(), $imageUrl),
         ]);
         // Blogger
         User::factory()->create([
             'name'      => 'blogger',
             'email'     => 'blogger@example.net',
             'password'  => Hash::make('12345678'),
-            'avatar'    => str_replace("{seed}", fake()->uuid(), $imageUrl),
         ]);
         // Other users
         User::factory(10)->create();
